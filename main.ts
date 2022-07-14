5bdd3d01f21ad0cbfb86788e9eef378b6cfcce3c
Pulkit <pulkityadav1@gmail.com>
2022-07-14 02:45:06 -0700

    All done.

--- main.ts
+++ main.ts
@@ -6,12 +6,23 @@ interface MyPluginSettings {
 	s1: string;
 	s2: string;
 	s3: string;
+	s4: string;
+	s5: string;
+	s6: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
 	s1: '🔥',
 	s2: '🔺',
-	s3: '🔸'
+	s3: '🔸',
+	s4: '✔︎ ',
+	s5: '❌ ',
+	s6: '❓'
+}
+
+interface SelectionRange {
+	start: { line: number; ch: number };
+	end: { line: number; ch: number };
 }
 
 export default class MyPlugin extends Plugin {
@@ -28,6 +39,36 @@ export default class MyPlugin extends Plugin {
 			callback: () => this.setTaskStatus(this.settings.s1)
 		});
 		
+		this.addCommand({
+			id: 'set-status-2',
+			name: 'Set task status 2',
+			callback: () => this.setTaskStatus(this.settings.s2)
+		});
+
+		this.addCommand({
+			id: 'set-status-3',
+			name: 'Set task status 3',
+			callback: () => this.setTaskStatus(this.settings.s3)
+		});
+
+		this.addCommand({
+			id: 'set-status-4',
+			name: 'Set task status 4',
+			callback: () => this.setTaskStatus(this.settings.s4)
+		});
+
+		this.addCommand({
+			id: 'set-status-5',
+			name: 'Set task status 5',
+			callback: () => this.setTaskStatus(this.settings.s5)
+		});
+
+		this.addCommand({
+			id: 'set-status-6',
+			name: 'Set task status 6',
+			callback: () => this.setTaskStatus(this.settings.s6)
+		});
+
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
@@ -48,7 +89,13 @@ export default class MyPlugin extends Plugin {
 		editor.getDoc().setSelection(lineRange.start, lineRange.end);
 		let lineText = editor.getSelection();
 
-		let newText = lineText + status;
+		let pos = lineText.indexOf(']');
+		if (pos < 0) { return; }
+		pos++;
+		let prefix = lineText.substring(0, pos);
+		let suffix = lineText.substring(pos).replace(/^\s+/g, '').replace(/[^\x00-\x80]/g, '').replace(/^\s+/g, '');
+		let newText = prefix + ' ' + status + ' ' + suffix;
+
 		editor.replaceSelection(newText);
 	}
 
@@ -78,22 +125,6 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
@@ -120,5 +151,65 @@ class SampleSettingTab extends PluginSettingTab {
 					this.plugin.settings.s1 = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+				.setName('Symbol for status 2')
+				.setDesc('Symbol for status 2')
+				.addText(text => text
+					.setPlaceholder('🔥')
+					.setValue(this.plugin.settings.s2)
+					.onChange(async (value) => {
+						console.log('S2: ' + value);
+						this.plugin.settings.s2 = value;
+						await this.plugin.saveSettings();
+					}));
+		
+		new Setting(containerEl)
+					.setName('Symbol for status 3')
+					.setDesc('Symbol for status 3')
+					.addText(text => text
+						.setPlaceholder('🔥')
+						.setValue(this.plugin.settings.s3)
+						.onChange(async (value) => {
+							console.log('S3: ' + value);
+							this.plugin.settings.s3 = value;
+							await this.plugin.saveSettings();
+						}));
+
+		new Setting(containerEl)
+						.setName('Symbol for status 4')
+						.setDesc('Symbol for status 4')
+						.addText(text => text
+							.setPlaceholder('🔥')
+							.setValue(this.plugin.settings.s4)
+							.onChange(async (value) => {
+								console.log('S4: ' + value);
+								this.plugin.settings.s4 = value;
+								await this.plugin.saveSettings();
+							}));
+
+		new Setting(containerEl)
+							.setName('Symbol for status 5')
+							.setDesc('Symbol for status 5')
+							.addText(text => text
+								.setPlaceholder('🔥')
+								.setValue(this.plugin.settings.s5)
+								.onChange(async (value) => {
+									console.log('S5: ' + value);
+									this.plugin.settings.s5 = value;
+									await this.plugin.saveSettings();
+								}));
+					
+		new Setting(containerEl)
+								.setName('Symbol for status 6')
+								.setDesc('Symbol for status 6')
+								.addText(text => text
+									.setPlaceholder('🔥')
+									.setValue(this.plugin.settings.s6)
+									.onChange(async (value) => {
+										console.log('S6: ' + value);
+										this.plugin.settings.s6 = value;
+										await this.plugin.saveSettings();
+									}));
 	}
 }
